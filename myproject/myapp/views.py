fdb9037bdecf487e0c6cb6e33ac5d0a050b2fc25
AneesAla <93885287+AneesAla@users.noreply.github.com>
2024-04-05 03:04:21 +0000

    new test page

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
 
+
 def home(request):
-    return render(request, 'home.html')
+  return render(request, 'home.html')
+
+
+def testpage(request):
+  return render(request, 'myapp/testpage.html')
